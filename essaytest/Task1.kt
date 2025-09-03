b5afbeaf12b4c507970256c9b81efcb02aaa18a4
hunggo <92900556+hunggo@users.noreply.github.com>
2025-09-03 09:14:31 +0700

    bổ xung task 1,2

--- essaytest/Task1.kt
+++ essaytest/Task1.kt
@@ -1,20 +1,18 @@
-package com.apero.essaytest
-
 fun main(){
-    bai1()
-    bai2()
-    bai3()
-    bai4()
-    bai5()
-    bai6()
-    bai7()
-    bai8()
-    bai9()
-    bai10()
-    bai11()
+    variable()
+    operator()
+    typeCasting()
+//    bai4()
+//    bai5()
+//    bai6()
+//    bai7()
+//    bai8()
+//    bai9()
+//    bai10()
+//    bai11()
 }
 // khai bao bien và hằng số
-fun bai1(){
+fun variable(){
     val name: String = "Hung"  //Hằng số không đổi
     var age: Int = 22   //hang so co the thay doi
     var gpa: Float = 7.5f   //diem trung binh
@@ -22,28 +20,58 @@ fun bai1(){
 
 }
 //Toán tu
-fun bai2(){
+fun operator(){
     var a = 10
     var b = 3
-    println("Tổng 2 số: ${a + b}")
-    println("Hiệu 2 số: ${a - b}")
-    println("Tích 2 số: ${a * b}")
-    println("Thương 2 số: ${a / b}")
-    println("Phép chia dư: ${a % b}")
-
+//    println("Tổng 2 số: ${a + b}")
+    println("Tổng 2 số: ${a.plus(b)}")
+//    println("Hiệu 2 số: ${a - b}")
+    println("Hiệu 2 số: ${a.minus(b)}")
+//    println("Tích 2 số: ${a * b}")
+    println("Tích 2 số: ${a.times(b)}")
+//    println("Thương 2 số: ${a / b}")
+    println("Thương phần nguyên: ${a.div(b)}")
+//    println("Phép chia dư: ${a % b}")
+    println("Thương lấy phần dư: ${a.mod(b)}")
     a += 5
     println("Sau khi a += 5: $a")
     b -= 2
     println("Sau khi b -= 2: $b")
+    if (a.compareTo(b) >= 0){
+        println("a là phần tử lớn hơn")
+        a
+    }else{
+        println("b là phần tử lớn hơn")
+        b
+    }
+    if (a.equals(b) ?: (b === null)){
+        println("a bằng b")
+    }else{
+        println("a khác b")
+    }
 }
 
 // ép kiểu
-fun bai3(){
+fun typeCasting(){
     val number: Double = 9.75
     val intNumber: Int = number.toInt()
     val stringNumber: String = number.toString()
 
+//    val unsafe: String = null as String   //unsafe
+    val unsafe: String? = null as String?
+    val safe: String? = number as? String // bên phải không thể null, kế =t quả của ép kiểu có thể null
     println("Kiểu double: $number, Kiểu Int: $intNumber, Kiểu String: $stringNumber")
+
+    if (stringNumber !is String || stringNumber.length == 0)
+        return
+    if (stringNumber is String && stringNumber.length > 0 ){
+        println("Chuỗi stringNumber có độ dài là: ${stringNumber.length}")
+    }
+
+    println(unsafe)
+    when(safe){
+        is String -> println(safe.length + 1)
+    }
 }
 
 //Dạng string teamplate
